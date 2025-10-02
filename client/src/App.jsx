af1c98c66961eeaf28d3ee18f5b74c62f30340b2
Dinhviet <vietphamdinhvan@gmail.com>
2025-10-02 23:52:49 +0700

    initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Header from './components/Header'
+import Home from './pages/Home'
+import Collection from './pages/Collection'
+import { Routes, Route } from 'react-router-dom'
+import { Router } from 'react-router-dom'
+
+function App() {
+  return (
+    <main> 
+      <Header/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/collection' element={<Collection/>}/>
+      </Routes>
+    </main>
+  )
+}
+
+export default App
---
+++ client/src/pages/Collection.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Collection() {
+  return (
+    <div>Collection Page</div>
+  )
+}
+
+export default Collection
