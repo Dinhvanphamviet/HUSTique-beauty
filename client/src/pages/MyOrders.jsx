ac4d103c47c3349ae473128602293b257807b5dc
Dinhviet <vietphamdinhvan@gmail.com>
2025-10-10 23:02:00 +0700

    Enhance MyOrders component layout and add order details including date, total amount, and status

--- client/src/pages/MyOrders.jsx
+++ client/src/pages/MyOrders.jsx
@@ -20,20 +20,20 @@ const MyOrders = () => {
 
 
   return (
-    <div>
+    <div className='max-padd-container py-16 pt-28 bg-primary'>
       <Title title1={"Thông tin"} title2={"Vận chuyển"} titleStyles={"pb-10"} />
       {orders.map((order) => (
-        <div key={order._id}>
+        <div key={order._id} className='bg-white p-2 mt-3 rounded-2xl'>
           {/* Order Items */}
           {order.items.map((item, idx) => (
-            <div key={idx}>
-              <div>
-                <div>
+            <div key={idx} className='text-gray-600 flex flex-col lg:flex-row gap-4 mb-3'>
+              <div className='flex flex-[2] gap-x-3'>
+                <div className='flexCenter bg-primary rounded-xl'>
                   <img src={item.product.images[0]} alt="" className='max-h-20 max-w-20 object-contain' />
                 </div>
                 <div className='block w-full'>
                   <h5 className='h5 uppercase line-clamp-1'>{item.product.title}</h5>
-                  <div>
+                  <div className='flex flex-wrap gap-3 max-sm:gap-y-1 mt-1'>
                     <div className='flex items-center gap-x-2'>
                       <h5 className='medium-14'>Gái:</h5>
                       <p>{currency}{item.product.price[item.size]}</p>
@@ -52,8 +52,8 @@ const MyOrders = () => {
             </div>
           ))}
           {/* Order Summary*/}
-          <div>
-            <div>
+          <div class="flex flex-col lg:flex-row justify-between items-start lg:items-center gap-4 border-t border-gray-300 pt-3">
+            <div className='flex flex-col gap-2'>
               <div className='flex items-center gap-x-2'>
                 <h5 className='medium-14'>Size:</h5>
                 <p className='text-gray-400 text-xs break-all'>{order._id}</p>
@@ -68,6 +68,26 @@ const MyOrders = () => {
                   </div>
                 </div>
               </div>
+              <div className='flex gap-4'>
+                <div className='flex items-center gap-x-2'>
+                  <h5 className='medium-14'>Ngày:</h5>
+                  <p className='text-gray-400 text-sm'>{new Date(order.createdAt).toDateString()}</p>
+                </div>
+                <div className='flex items-center gap-x-2'>
+                  <h5 className='medium-14'>Tổng tiền:</h5>
+                  <p className='text-gray-400 text-xs break-all'>{currency}{order.amount}</p>
+                </div>
+              </div>
+            </div>
+            <div className='flex gap-3'>
+              <div className='flex items-center gap-2'>
+                <h5 className='medium-14'>Trạng thái:</h5>
+                <div className='flex items-center gap-1'>
+                  <span className='min-w-2 h-2 rounded-full bg-green-500' />
+                <p>{order.status}</p>
+                </div>
+              </div>
+              <button className='btn-secondary !py-1 !text-xs rounded-sm'>Theo dõi đơn hàng </button>
             </div>
           </div>
         </div>
