6ad0583f224ccf133e74ab46d09b66d98bb0788b
Dinhviet <vietphamdinhvan@gmail.com>
2025-10-13 00:00:05 +0700

    Remove node_modules from tracking

---
+++ server/server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+
+const app = express() //Init express app
+app.use(cors()) //Enable Cross-Origin Resource Sharing
+
+
+//Routes Endpoint to check API Status
+app.get('/', (req, res) => {
+    res.send("API Successfull connected...")
+})
+
+const port = process.env.PORT || 3000 //Define server port
+
+//Start server
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`)
+})
