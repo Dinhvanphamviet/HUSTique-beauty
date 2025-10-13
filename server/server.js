3a6f2736a1a7bd5bce20e4219b62a29d93ab7873
Dinhviet <vietphamdinhvan@gmail.com>
2025-10-13 09:19:49 +0700

    Add Clerk middleware

--- server/server.js
+++ server/server.js
@@ -2,11 +2,16 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import connectDB from './config/mongodb.js'
+import { clerkMiddleware } from '@clerk/express'
+
 
 await connectDB() //Connect to MongoDB
 
 const app = express() //Init express app
 app.use(cors()) //Enable Cross-Origin Resource Sharing
+
+app.use(express.json()) //Enable JSON body parsing
+app.use(clerkMiddleware()) //Enable Clerk middleware for authentication
 
 
 //Routes Endpoint to check API Status
