b6f4089216f643683f4eb9e34f6b57aeb4532550
Dinhviet <vietphamdinhvan@gmail.com>
2025-10-19 22:29:24 +0700

    fix connect db

--- server/config/mongodb.js
+++ server/config/mongodb.js
@@ -3,9 +3,9 @@ import mongoose from "mongoose";
 const connectDB = async () => {
     try {
         await mongoose.connect(`${process.env.MONGO_URI}`)
-        console.log("✔️ Database connected")
+        console.log("Database connected")
     } catch (error) {
-        console.log("❌ Database connection failed: ", error.message)
+        console.log("Database connection failed: ", error.message)
     }
 }
 
